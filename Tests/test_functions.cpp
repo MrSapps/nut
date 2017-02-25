a6dba1e596fa1082d3ca71fd34e843897ac06b14
Paul <Paul@paul.com>
2017-02-25 17:12:25 +0000

    start to draft interface

--- Tests/test_functions.cpp
+++ Tests/test_functions.cpp
@@ -3,7 +3,19 @@
 #include <catch.hpp>
 #include <nut/nut.hpp>
 
-TEST_CASE("functions", "check calling function with no arguments and no return") 
+bool function_with_no_arguments_and_no_return_value_called = false;
+void function_with_no_arguments_and_no_return_value()
 {
-    REQUIRE(1 == 2);
+    function_with_no_arguments_and_no_return_value_called = true;
+}
+
+TEST_CASE("functions", "check calling function with no arguments and no return value") 
+{
+    nut::vm vm;
+
+    vm.set_function("function_with_no_arguments_and_no_return_value", function_with_no_arguments_and_no_return_value);
+
+    REQUIRE(function_with_no_arguments_and_no_return_value_called == false);
+    vm.script("function_with_no_arguments_and_no_return_value()");
+    REQUIRE(function_with_no_arguments_and_no_return_value_called == true);
 }
--- include/nut/nut.hpp
+++ include/nut/nut.hpp
@@ -1,2 +1,85 @@
 #pragma once
 
+#include <squirrel.h>
+#include <sqstdio.h> 
+#include <sqstdaux.h> 
+
+namespace nut
+{
+    class stack_saver
+    {
+    public:
+        stack_saver(HSQUIRRELVM hVm)
+            : mVm(hVm)
+        {
+            mTop = sq_gettop(mVm);
+        }
+
+        ~stack_saver()
+        {
+            sq_settop(mVm, mTop);
+        }
+    private:
+        HSQUIRRELVM mVm;
+        int mTop;
+    };
+
+    class vm
+    {
+    public:
+        vm()
+        {
+            const SQInteger kInitialStackSize = 1024;
+            mVmHandle = sq_open(kInitialStackSize);
+            sq_setforeignptr(mVmHandle, this);
+            sqstd_seterrorhandlers(mVmHandle);
+            sq_setprintfunc(mVmHandle, printfunc, errfunc);
+        }
+
+        ~vm()
+        {
+            if (mVmHandle)
+            {
+                sq_close(mVmHandle);
+            }
+        }
+
+        template<class FunctionType>
+        void set_function(const char* functionName, FunctionType func)
+        {
+
+        }
+
+        void script(const char* script)
+        {
+            stack_saver ss(mVmHandle);
+        }
+
+    private:
+        vm* ThisFromVm(HSQUIRRELVM vm)
+        {
+            return reinterpret_cast<nut::vm*>(sq_getforeignptr(vm));
+        }
+
+        static void errfunc(HSQUIRRELVM /*v*/, const SQChar* s, ...)
+        {
+            char buffer[1024] = {};
+            va_list arglist;
+            va_start(arglist, s);
+            vsprintf(buffer, s, arglist);
+            va_end(arglist);
+        }
+
+        static void printfunc(HSQUIRRELVM /*v*/, const SQChar* s, ...)
+        {
+            char buffer[1024] = {};
+            va_list arglist;
+            va_start(arglist, s);
+            vsprintf(buffer, s, arglist);
+            va_end(arglist);
+        }
+
+    public:
+        HSQUIRRELVM mVmHandle = nullptr;
+    };
+}
